c730e34fba9111b9bad4c28dcb6ca3342207ab1e
gokhanc <gokuclskn@gmail.com>
2022-03-31 21:21:38 +0100

    the red car is ready for the trial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,12 @@ public class Main {
         // the basic structure
         CarBasics car=new CarBasics();
 
+        car.color="red";
+        car.engine="7999cc";
+        car.door="2";
+        car.number="1123";
+        System.out.println(car.color);
+
 
 
     }
